1c62d7adf315260a9ea1c757cea8a0f2347dcbf0
Eric Wollesen <eric@leadtune.com>
2011-06-13 17:37:41 -0600

    Move from RAILS_ENV to Rails.env

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'ostruct'
 
-::OrbitedConfig = OpenStruct.new(YAML.load_file(File.join(RAILS_ROOT, 'config', 'orbited.yml'))[RAILS_ENV])
+::OrbitedConfig = OpenStruct.new(YAML.load_file(File.join(Rails.root.to_s, 'config', 'orbited.yml'))[Rails.env])
 
 ActionView::Base.send :include, OrbitedHelper
 
